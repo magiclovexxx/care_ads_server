e951533262dd3f915b18897a338c21b4aa587366
quangnguyen7616 <84000455+quangnguyen7616@users.noreply.github.com>
2021-05-12 11:10:07 +0700

    Shopee
    Update shopee api

--- api/ads_shopee.js
+++ api/ads_shopee.js
@@ -1,64 +1,158 @@
-const axios = require('axios').default;
-const puppeteer = require('puppeteer');
-const randomUseragent = require('random-useragent');
+const md5 = require('md5');
+const crypto = require('crypto');
 
-
-const loginShopee = async (usser, passs) => {
-    // lay cookie
-    cookie = "111"
-
-
-    // trả về cookie kênh người bán
-    return cookie
-
+function createAxios() {
+    const axios = require('axios');
+    return axios.create({ withCredentials: true });
 }
 
-loginShopeeWithFaCode = async (usser, passs, facode) => {
-    cookie1 = ""
-
-    var data = JSON.stringify({ "shopid": shopId });
-
-    var config = {
-        method: 'post',
-        url: 'https://shopee.vn/api/v4/shop/follow',
-        headers: {
-            'content-type': 'application/json',
-            'referer': ref,
-            'cookie': cookie1
-        },
-        data: data
-    };
-
-    axios(config)
-        .then(function (response) {
-            console.log(response.data);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-
-
+function cookieParse(cookie)
+{
+    var result = [];
+    for(var i = 0; i < cookie.length; i++)
+        result.push(cookie[i].split(';')[0]);
+    return result.join('; ');
 }
-function csrftoken() {
-    karakter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    PanjangKarakter = karakter.length;
-    acakString = '';
-    for (let i = 0; i < 32; i++) {
-        PanjangKarakter = PanjangKarakter - 1
-        acakString += karakter[Math.floor(Math.random() * (PanjangKarakter))];
 
+const axiosInstance = createAxios();
 
+const api_login = async (SPC_CDS, SPC_CDS_VER, cookie, username, phone, password, remember, vcode) => {        
+    const password_hash = crypto.createHash('sha256').update(md5(password)).digest('hex');
+    const Url = 'https://banhang.shopee.vn/api/v2/login/?SPC_CDS=' + SPC_CDS + '&SPC_CDS_VER=' + SPC_CDS_VER;
+    var data = (username != null ? 'username=' + username : 'phone=' + phone);
+    data += '&password_hash=' + password_hash;
+    data += '&remember=' + (remember ? 'true' : 'false');
+    if(vcode != null)
+    {
+        data += '&vcode=' + vcode;
     }
-    return acakString;
+    const result = await axiosInstance.post(Url, data).then(function(response) {  
+        return { status: response.status, data: response.data, cookie: response.headers['set-cookie'] };
+    }).catch(function(error) {
+        if (error.response) {
+            return { status: error.response.status, data: error.response.data, cookie: null };
+        }
+        else {
+            return { status: -1, data: null, cookie: null };
+        }
+    });
+    return result;
 }
 
+const api_get_all_category_list = async (SPC_CDS, SPC_CDS_VER, cookie, version) => {        
+    const Url = 'https://banhang.shopee.vn/api/v3/category/get_all_category_list/?SPC_CDS=' + SPC_CDS + '&SPC_CDS_VER=' + SPC_CDS_VER + '&version=' + version;
+    const result = await axiosInstance.get(Url, {
+        headers: {
+            cookie: cookieParse(cookie),
+        }
+    }).then(function(response) {  
+        return { status: response.status, data: response.data };
+    }).catch(function(error) {
+        if (error.response) {
+            return { status: error.response.status, data: error.response.data };
+        }
+        else {
+            return { status: -1, data: null, cookie: null };
+        }
+    });
+    return result;
+}
 
+const api_get_second_category_list = async (SPC_CDS, SPC_CDS_VER, cookie, version) => {        
+    const Url = 'https://banhang.shopee.vn/api/v3/category/get_second_category_list/?SPC_CDS=' + SPC_CDS + '&SPC_CDS_VER=' + SPC_CDS_VER + '&version=' + version;
+    const result = await axiosInstance.get(Url, {
+        headers: {
+            cookie: cookieParse(cookie),
+        }
+    }).then(function(response) {  
+        return { status: response.status, data: response.data };
+    }).catch(function(error) {
+        if (error.response) {
+            return { status: error.response.status, data: error.response.data };
+        }
+        else {
+            return { status: -1, data: null, cookie: null };
+        }
+    });
+    return result;
+}
 
+const api_shopcategory = async (SPC_CDS, SPC_CDS_VER, cookie, page_number, page_size) => {        
+    var Url = 'https://banhang.shopee.vn/api/shopcategory/v3/category/page_active_collection_list/?SPC_CDS=' + SPC_CDS + '&SPC_CDS_VER=' + SPC_CDS_VER;
+    Url += '&page_number=' + page_number;
+    Url += '&page_size=' + page_size;
 
+    const result = await axiosInstance.get(Url, {
+        headers: {
+            cookie: cookieParse(cookie),
+        }
+    }).then(function(response) {  
+        return { status: response.status, data: response.data };
+    }).catch(function(error) {
+        if (error.response) {
+            return { status: error.response.status, data: error.response.data };
+        }
+        else {
+            return { status: -1, data: null, cookie: null };
+        }
+    });
+    return result;
+}
+
+const api_product_selector = async (SPC_CDS, SPC_CDS_VER, cookie, offset, limit, is_ads, need_brand, need_item_model, search_type, search_content) => {        
+    var Url = 'https://banhang.shopee.vn/api/marketing/v3/public/product_selector/?SPC_CDS=' + SPC_CDS + '&SPC_CDS_VER=' + SPC_CDS_VER;
+    Url += '&offset=' + offset;
+    Url += '&limit=' + limit;
+    Url += '&is_ads=' + is_ads;
+    Url += '&need_brand=' + need_brand;
+    Url += '&need_item_model=' + need_item_model;
+    if (search_type != null) {
+        Url += '&search_type=' + search_type;
+        Url += '&search_content=' + search_content;        
+    }
+
+    const result = await axiosInstance.get(Url, {
+        headers: {
+            cookie: cookieParse(cookie),
+        }
+    }).then(function(response) {  
+        return { status: response.status, data: response.data };
+    }).catch(function(error) {
+        if (error.response) {
+            return { status: error.response.status, data: error.response.data };
+        }
+        else {
+            return { status: -1, data: null, cookie: null };
+        }
+    });
+    return result;
+}
+
+const api_get_item_status = async (SPC_CDS, SPC_CDS_VER, cookie, item_id_list) => {
+    const Url = 'https://banhang.shopee.vn/api/marketing/v3/pas/get_item_status/?SPC_CDS=' + SPC_CDS + '&SPC_CDS_VER=' + SPC_CDS_VER;
+    const result = await axiosInstance.post(Url, item_id_list, {
+        headers: {
+            cookie: cookieParse(cookie),
+        }
+    }).then(function(response) {  
+        return { status: response.status, data: response.data };
+    }).catch(function(error) {
+        if (error.response) {
+            return { status: error.response.status, data: error.response.data };
+        }
+        else {
+            return { status: -1, data: null, cookie: null };
+        }
+    });
+    return result;
+}
 
 module.exports = {
 
-    loginShopee,
-    loginShopeeWithFaCode,
-    
+    api_login,
+    api_get_all_category_list,
+    api_get_second_category_list,
+    api_shopcategory,
+    api_product_selector,
+    api_get_item_status,
 }
