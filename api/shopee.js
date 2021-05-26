324ca73a32f1ad2a2c828d0e95d6934fd991743c
love magic <magic.loveptit@gmail.com>
2021-05-26 17:49:29 +0700

    fb

--- api/shopee.js
+++ api/shopee.js
@@ -1,26 +1,65 @@
-const puppeteer = require('puppeteer');
+const md5 = require('md5');
+const crypto = require('crypto');
+const publicIp = require('public-ip');
+var shopeeApi = require('../api/ads_shopee.js');
+
+function createAxios() {
+    const axios = require('axios');
+    return axios.create({ withCredentials: true });
+}
+
+function cookieParse(cookie) {
+    var result = [];
+    for (var i = 0; i < cookie.length; i++)
+        result.push(cookie[i].split(';')[0]);
+    return result.join('; ');
+}
+
+const axiosInstance = createAxios();
+
+//Api shopee
 
 
-getTuKhoaGoiY = async (key) => {
-    const browser = await puppeteer.launch({
-        headless: true,
-        devtools: false,
-    });
+const shopee_cron_check = async(SPC_CDS, UserAgent, cookie) => { 
+    console.log(await publicIp.v4());
+	// Lấy thông tin tài khoản shopee thuộc sv
 
-    width = Math.floor(Math.random() * (1280 - 1000)) + 1000;;
-    height = Math.floor(Math.random() * (800 - 600)) + 600;;
-    const page = (await browser.pages())[0];
 
-    try {
-        await page.goto("https://gauth.apps.gbraad.nl/")
+}
 
-       
-        return code2fa
-    } catch (error) {
-        console.log(error)
-        await browser.close();
-        return "Có lỗi gì đó"
-
+const shopee_check_ads = async(SPC_CDS, UserAgent, cookie) => {
+   
+    const Url = 'https://banhang.shopee.vn/api/v2/login/?SPC_CDS=' + SPC_CDS + '&SPC_CDS_VER=2';
+    cookie = cookie.split('; ')
+    for (var i = 0; i < cookie.length; i++) {
+        if (cookie[i].indexOf('SPC_EC=') != -1) {
+            cookie = cookie[i];
+            break;
+        }
     }
+    const result = await axiosInstance.get(Url, {
+        headers: {
+            cookie: cookie,
+            'User-Agent': UserAgent
+        }
+    }).then(function(response) {
+        response.data.cookie = cookieParse(response.headers['set-cookie']) + '; ' + cookie;
+        response.data.status = response.status;
+        return response.data;
+    }).catch(function(error) {
+        if (error.response) {
+            error.response.data.status = error.response.status;
+            return error.response.data;
+        } else {
+            return null;
+        }
+    });
+    return result;
+}
+
+
+module.exports = {
+    shopee_check_ads, //check ads,
+    shopee_cron_check // cron
 
 }
