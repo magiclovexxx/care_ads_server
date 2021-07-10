576f72df9dfaf1a5bdc9f8ea03ea8d861ec9b3fb
root <root@static.vnpt.vn>
2021-07-10 19:24:09 +0700

    update

--- api/ads_shopee.js
+++ api/ads_shopee.js
@@ -4,7 +4,7 @@ var moment = require('moment');
 
 function createAxios() {
     const axios = require('axios');
-    return axios.create({ withCredentials: true, timeout: 120000 });
+    return axios.create({ withCredentials: true, timeout: 15000 });
 }
 
 function cookieParse(cookie, cookie_array) {
