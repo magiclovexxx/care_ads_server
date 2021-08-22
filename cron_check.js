c1d4c31fbec3092495c06ee56b82a05a4050d073
Quang Nguyen <quangnguyen7616@gmail.com>
2021-08-22 21:36:50 +0700

    update

--- cron_check.js
+++ cron_check.js
@@ -159,6 +159,7 @@ async function locationKeyword(shopname, shopid, campaignid, itemid, max_page, p
     let result = await shopeeApi.api_get_search_items_waiting(proxy, user_agent, cookie, by, keyword, limit, newest, order, 'search', 'PAGE_GLOBAL_SEARCH', 2);
     if (result.code != 0) {
         if (result.code == 1000) {
+            console.log(moment().format('MM/DD/YYYY HH:mm:ss'), '(' + shopname + ' -> ' + campaignid + ') Lỗi api_get_search_items_waiting', result);
             sleep(100);
             return locationKeyword(shopname, shopid, campaignid, itemid, max_page, proxy, cookie, by, keyword, limit, newest, order);
         } else {
