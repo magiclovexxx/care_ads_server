cf4d7a1879537367c930311254d45c4bcdafd41b
Quang Nguyen <quangnguyen7616@gmail.com>
2021-08-23 11:36:29 +0700

    update

---
+++ test_request.js
@@ -0,0 +1,36 @@
+const { v4: uuidv4 } = require('uuid');
+const prompt = require('prompt');
+var shopeeApi = require('./api/ads_shopee.js');
+var moment = require('moment');
+const NodeRSA = require('node-rsa');
+function createAxios() {
+    const axios = require('axios');
+    return axios.create({ withCredentials: true, timeout: 60000 });
+}
+const axiosInstance = createAxios();
+
+function request_sacuco(Url) {    
+    return axiosInstance.get(Url).then(function (response) {
+        response.data.status = response.status;
+        return response.data;
+    }).catch(function (error) {
+        if (error.response) {
+            error.response.data.status = error.response.status;
+            return error.response.data;
+        } else {
+            return { code: 1000, message: error.code + ' ' + error.message };
+        }
+    });
+}
+
+(async () => {
+    let user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4557.4 Safari/537.36';
+    const { url } = await prompt.get(['url']);
+    const { forcount } = await prompt.get(['forcount']);
+    for (let i = 0; i < forcount; i++) {
+        let start_unix = moment().unix();
+        let result = await request_sacuco(url);
+        let end_unix = moment().unix();
+        console.log(moment().format('MM/DD/YYYY HH:mm:ss'), 'Kết quả truy vấn:', i, (end_unix - start_unix) + 's');
+    }
+})();
--- test_search.js
+++ test_search.js
@@ -5,19 +5,13 @@ var moment = require('moment');
 const NodeRSA = require('node-rsa');
 
 (async () => {
-    while (true) {
-        const { keyword } = await prompt.get(['keyword']);
-        if (keyword != 'exit') {            
-            let user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4557.4 Safari/537.36';
-            const { forcount } = await prompt.get(['forcount']);
-            for (let i = 0; i < forcount; i++) {
-                let start_unix = moment().unix();
-                let result = await shopeeApi.api_get_search_items_waiting(null, user_agent, null, 'relevancy', keyword, 60, 0, 'desc', 'search', 'PAGE_GLOBAL_SEARCH', 2);
-                let end_unix = moment().unix();
-                console.log(moment().format('MM/DD/YYYY HH:mm:ss'), 'Tìm vị trí:', i, keyword, result.message, (end_unix - start_unix) + 's');
-            }
-        } else {
-            break;
-        }
+    const { keyword } = await prompt.get(['keyword']);
+    let user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4557.4 Safari/537.36';
+    const { forcount } = await prompt.get(['forcount']);
+    for (let i = 0; i < forcount; i++) {
+        let start_unix = moment().unix();
+        let result = await shopeeApi.api_get_search_items_waiting(null, user_agent, null, 'relevancy', keyword, 60, 0, 'desc', 'search', 'PAGE_GLOBAL_SEARCH', 2);
+        let end_unix = moment().unix();
+        console.log(moment().format('MM/DD/YYYY HH:mm:ss'), 'Tìm vị trí:', i, keyword, result.message, (end_unix - start_unix) + 's');
     }
 })();
