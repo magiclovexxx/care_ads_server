0844667c348524d9e41499106190f88f30169d51
Quang Nguyen <quangnguyen7616@gmail.com>
2021-09-02 19:59:43 +0700

    update

--- cron_check.js
+++ cron_check.js
@@ -380,10 +380,9 @@ check_all = async () => {
                         return;
                     }
                 }
-                return;
                 //Check restore
                 let is_restore_check = false;
-
+                /*
                 //Lấy đơn hàng hủy
                 let cancel_page = 1;
                 let count_cancel_page = 0;
@@ -853,7 +852,7 @@ check_all = async () => {
                         break;
                     }
                 }
-
+                */
                 //Lấy thanh toán về ví
                 let pay_page = 1;
                 let count_pay_page = 0;
