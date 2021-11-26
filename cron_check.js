aa4b0751a3d2fac5e9e07c7782e97090b2d8132a
Quang Nguyen <quangnguyen7616@gmail.com>
2021-11-26 09:29:27 +0700

    update

--- cron_check.js
+++ cron_check.js
@@ -208,7 +208,6 @@ function getMaxPage(max_location) {
 }
 
 async function locationKeyword(shopname, shopid, campaignid, itemid, max_page, proxy, cookie, user_agent, by, keyword, limit, newest, order) {
-    return 999;
     let start_unix = moment().unix();
     let result = await shopeeApi.api_get_search_items(proxy, user_agent, cookie, by, keyword, limit, newest, order, 'search', 'PAGE_GLOBAL_SEARCH', 2);
     last_request_success = moment();
@@ -1614,7 +1613,7 @@ check_all = async () => {
                                                 let old_price = keyword.price;
                                                 let ads_location = 999;
                                                 if (keyword.price == max_price) {
-                                                    if (moment(care_keyword.last_check_time).add(180, 'minutes') < moment()) {
+                                                    if (moment(care_keyword.last_check_time).add(30, 'minutes') < moment()) {
                                                         ads_location = await locationKeyword(campaign.name, campaign.shop_id, campaign.campaignid, itemid, 0, null, null, clone_user_agent, 'relevancy', keyword.keyword, 60, 0, 'desc');
                                                         last_request_success = moment();
                                                         is_next_step = await php_update_placements(campaign, [{
@@ -1758,7 +1757,7 @@ check_all = async () => {
                                                         let old_price = keyword.price;
                                                         let ads_location = 999;
                                                         if (keyword.price == max_price) {
-                                                            if (moment(care_keyword.last_check_time).add(180, 'minutes') < moment()) {
+                                                            if (moment(care_keyword.last_check_time).add(30, 'minutes') < moment()) {
                                                                 ads_location = await locationKeyword(campaign.name, campaign.shop_id, campaign.campaignid, itemid, 0, null, null, clone_user_agent, 'relevancy', keyword.keyword, 60, 0, 'desc');
                                                                 last_request_success = moment();
                                                                 is_next_step = await php_update_placements(campaign, [{
