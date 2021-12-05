8cb710890ed1cffa248d7ab52ae091f2f6b12bfa
Quang Nguyen <quangnguyen7616@gmail.com>
2021-12-05 19:15:04 +0700

    update

--- cron_check.js
+++ cron_check.js
@@ -217,7 +217,7 @@ async function locationKeyword(shopname, shopid, campaignid, itemid, max_page, p
 }
 
 async function locationKeyword_Atosa(shopname, shopid, campaignid, itemid, max_page, proxy, cookie, user_agent, by, keyword, limit, newest, order) {
-    const time_out = Math.floor(Math.random() * (5555 - 3333 + 1)) + 3333;
+    const time_out = Math.floor(Math.random() * (9999 - 3333 + 1)) + 3333;
     await sleep(time_out);
     let start_unix = moment().unix();
     let result = await shopeeApi.api_get_search_items_atosa(proxy, user_agent, cookie, by, keyword, limit, newest, order, 'search', 'PAGE_GLOBAL_SEARCH', 2);
@@ -274,7 +274,7 @@ async function locationKeyword_Atosa(shopname, shopid, campaignid, itemid, max_p
 }
 
 async function locationKeyword_SaleWork(shopname, shopid, campaignid, itemid, max_page, proxy, cookie, user_agent, by, keyword, limit, newest, order) {
-    const time_out = Math.floor(Math.random() * (5555 - 3333 + 1)) + 3333;
+    const time_out = Math.floor(Math.random() * (9999 - 3333 + 1)) + 3333;
     await sleep(time_out);
     let start_unix = moment().unix();
     let result = await shopeeApi.api_get_search_items_salework(keyword, itemid);
@@ -305,7 +305,7 @@ async function locationKeyword_SaleWork(shopname, shopid, campaignid, itemid, ma
 }
 
 async function locationKeyword_Shopee(shopname, shopid, campaignid, itemid, max_page, proxy, cookie, user_agent, by, keyword, limit, newest, order) {
-    const time_out = Math.floor(Math.random() * (5555 - 3333 + 1)) + 3333;
+    const time_out = Math.floor(Math.random() * (9999 - 3333 + 1)) + 3333;
     await sleep(time_out);
     let start_unix = moment().unix();
     let result = await shopeeApi.api_get_search_items(proxy, user_agent, cookie, by, keyword, limit, newest, order, 'search', 'PAGE_GLOBAL_SEARCH', 2);
