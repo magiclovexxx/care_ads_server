8f58e1932688079e792efb23594676eee8fb2e21
Quang Nguyen <quangnguyen7616@gmail.com>
2021-12-08 17:04:11 +0700

    update

--- cron_check.js
+++ cron_check.js
@@ -283,9 +283,8 @@ async function locationKeyword_ShopeeV2(shopname, shopid, campaignid, itemid, ma
         }
     }
     last_request_success = moment();
-    console.log(JSON.stringify(result));
-    if (result.data.data.items != null) {
-        let index = result.data.data.items.findIndex(x => x.itemid == itemid && x.shopid == shopid && x.campaignid != null);
+    if (result.data.items != null) {
+        let index = result.data.items.findIndex(x => x.itemid == itemid && x.shopid == shopid && x.campaignid != null);
         let page = (newest / limit);
         if (index != -1) {
             let ads_location = (index + 1);
