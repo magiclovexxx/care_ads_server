40bc3bc502622f3587edbea2f7ba187f2051f6ab
love magic <magiclove.loveptit@gmail.com>
2023-02-01 18:04:00 +0700

    test puppeteer

--- test.js
+++ test.js
@@ -1,11 +1,42 @@
 
-const ShopeeAPI = require('./api/ShopeeAPI.js');
-var api = new ShopeeAPI();
+// const ShopeeAPI = require('./api/ShopeeAPI.js');
+// var api = new ShopeeAPI();
 
-run = async () => {
-    const r = await api.api_get_search_items(null, null, null, null, 'Ví nữ', 60, 60, null, null, null, null, null);
-    console.log(r);
-}
+// run = async () => {
+//     const r = await api.api_get_search_items(null, null, null, null, 'Ví nữ', 60, 60, null, null, null, null, null);
+//     console.log(r);
+// }
 
 
-run();
+// run();
+
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto('https://developer.chrome.com/');
+
+  // Set screen size
+  await page.setViewport({width: 1080, height: 1024});
+
+  // Type into search box
+  await page.type('.search-box__input', 'automate beyond recorder');
+
+  // Wait and click on first result
+  const searchResultSelector = '.search-box__link';
+  await page.waitForSelector(searchResultSelector);
+  await page.click(searchResultSelector);
+
+  // Localte the full title with a unique string
+  const textSelector = await page.waitForSelector(
+    'text/Customize and automate'
+  );
+  const fullTitle = await textSelector.evaluate(el => el.textContent);
+
+  // Print the full title
+  console.log('The title of this blog post is "%s".', fullTitle);
+
+  await browser.close();
+})();
