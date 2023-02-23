43a1a480de23e444a7e70a567046c42a4dc2cb4c
love magic <magic.loveptit@gmail.com>
2023-02-23 09:09:17 +0700

    fix

--- cron_check.js
+++ cron_check.js
@@ -540,41 +540,42 @@ async function locationKeyword_Shopee(shopname, shopid, campaignid, itemid, max_
     let end_unix = moment().unix();
     
     if (result.code != 0) {
-        let statusCode = result.status;
-        //if (result.code == 1000) {
-        //if (result.status == 429 || result.status == 403) {
-        let is_waiting = false;
-        let old_ip = 'N';
-        if (proxy != null) {
-            old_ip = proxy.host;
-        }
-        while (change_proxy_pending) {
-            await sleep(3000);
-            is_waiting = true;
-        }
-        if (!is_waiting) {
-            change_proxy_pending = true;
-            result = await api_get_proxy_ip(slave_ip, old_ip);
-            if (result.code != 0) {
-                change_proxy_pending = false;
-                console.error(moment().format('MM/DD/YYYY HH:mm:ss'), 'Lỗi api_get_proxy_ip', result.message);
-                return -1;
-            }
-            proxy_server = {
-                host: result.data.proxy_ip,
-                port: parseInt(result.data.proxy_port),
-                auth: { username: result.data.proxy_username, password: result.data.proxy_password.replace('\r', '') }
-            };
-            change_proxy_pending = false;
-            console.error(moment().format('MM/DD/YYYY HH:mm:ss'), '(' + shopname + ' -> ' + campaignid + ') Shopee chặn nhiều request (' + statusCode + ') -> Đổi Proxy', old_ip, ' -> ', result.data.proxy_ip);
-        }
-        await sleep(10000);
-        return locationKeyword_Shopee(shopname, shopid, campaignid, itemid, max_page, proxy_server, cookie, user_agent, by, keyword, limit, newest, order);
-        /*} else {
-            console.error(moment().format('MM/DD/YYYY HH:mm:ss'), '(' + shopname + ' -> ' + campaignid + ') Shopee Request Timeout');
-            await sleep(10000);
-            return locationKeyword_Shopee(shopname, shopid, campaignid, itemid, max_page, proxy, cookie, user_agent, by, keyword, limit, newest, order);
-        }*/
+        return 999
+        // let statusCode = result.status;
+        // //if (result.code == 1000) {
+        // //if (result.status == 429 || result.status == 403) {
+        // let is_waiting = false;
+        // let old_ip = 'N';
+        // if (proxy != null) {
+        //     old_ip = proxy.host;
+        // }
+        // while (change_proxy_pending) {
+        //     await sleep(3000);
+        //     is_waiting = true;
+        // }
+        // if (!is_waiting) {
+        //     change_proxy_pending = true;
+        //     result = await api_get_proxy_ip(slave_ip, old_ip);
+        //     if (result.code != 0) {
+        //         change_proxy_pending = false;
+        //         console.error(moment().format('MM/DD/YYYY HH:mm:ss'), 'Lỗi api_get_proxy_ip', result.message);
+        //         return -1;
+        //     }
+        //     proxy_server = {
+        //         host: result.data.proxy_ip,
+        //         port: parseInt(result.data.proxy_port),
+        //         auth: { username: result.data.proxy_username, password: result.data.proxy_password.replace('\r', '') }
+        //     };
+        //     change_proxy_pending = false;
+        //     console.error(moment().format('MM/DD/YYYY HH:mm:ss'), '(' + shopname + ' -> ' + campaignid + ') Shopee chặn nhiều request (' + statusCode + ') -> Đổi Proxy', old_ip, ' -> ', result.data.proxy_ip);
+        // }
+        // await sleep(10000);
+        // return locationKeyword_Shopee(shopname, shopid, campaignid, itemid, max_page, proxy_server, cookie, user_agent, by, keyword, limit, newest, order);
+        // /*} else {
+        //     console.error(moment().format('MM/DD/YYYY HH:mm:ss'), '(' + shopname + ' -> ' + campaignid + ') Shopee Request Timeout');
+        //     await sleep(10000);
+        //     return locationKeyword_Shopee(shopname, shopid, campaignid, itemid, max_page, proxy, cookie, user_agent, by, keyword, limit, newest, order);
+        // }*/
     }
     last_request_success = moment();
     if (result.data.items != null) {
@@ -3076,6 +3077,7 @@ run = async () => {
             exec('rm -f core.*');
             exec('pm2 flush');
             exec('rm ~/.pm2/pm2.log');
+            exec('pm2 restart cron_check.js');
 
         } catch (error) {
             console.log(error)
